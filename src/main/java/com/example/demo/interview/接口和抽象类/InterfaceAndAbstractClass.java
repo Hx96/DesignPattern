b2471d6b524a6fa9205fbc45e4cc397a1f5dbe44
Hx96 <1014920159@qq.com>
2021-07-03 10:29:01 +0800

    sync

---
+++ src/main/java/com/example/demo/interview/接口和抽象类/InterfaceAndAbstractClass.java
@@ -0,0 +1,32 @@
+package com.example.demo.interview.接口和抽象类;
+
+/**
+ * @author XingHuang
+ */
+public class InterfaceAndAbstractClass {
+
+    /**
+     *
+     */
+    abstract class AbstractClass{
+        /**
+         * 被子类所继承
+         * @return
+         */
+        abstract public double area();
+
+
+    }
+
+
+    /**
+     * 接口 接口变量自动定义 public static final
+     * 抽象类的成员变量可以是各种类型的
+     */
+    interface DefaultDefine{
+        // idea 会变 黑
+        public static final int a = 0;
+
+    }
+
+}
